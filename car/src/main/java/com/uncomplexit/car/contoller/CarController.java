5d068dd612d496ad4a7c7d4af458c16d65662f2d
Shikhar Agarwal <shikharagarwal19@gmail.com>
2024-01-27 03:55:40 +0530

    Episode 2 Basic springboot project with mysql db Get Post call

--- car/src/main/java/com/uncomplexit/car/contoller/CarController.java
+++ car/src/main/java/com/uncomplexit/car/contoller/CarController.java
@@ -3,6 +3,7 @@ package com.uncomplexit.car.contoller;
 import com.uncomplexit.car.beans.Car;
 import com.uncomplexit.car.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +25,9 @@ public class CarController {
         return carService.getCarId(id);
     }
 
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Car createCar(@RequestBody Car car) {
         return carService.insertCar(car);
     }
